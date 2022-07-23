4aa3891f8cd03cf4ffa7fb2b309fe45d64c24b47
ppoliani <ppoliani@gmail.com>
2022-07-23 14:49:42 +0300

    feat: implement the create new retry consumer

--- src/consumer/retry_consumer.rs
+++ src/consumer/retry_consumer.rs
@@ -13,9 +13,9 @@ use crate::{
   core::connection::Connection,
 };
 
-pub struct RetryConsumer {
-  channel: Channel,
-}
+pub struct RetryConsumer;
+
+static ANY_MESSAGE: &str = "#";
 
 impl RetryConsumer {
   pub async fn new(
@@ -23,26 +23,49 @@ impl RetryConsumer {
     exchange_name: &str,
     queue_name: &str,
     binding_key: &str,
+    ttl: u64,
   ) -> Self {
     let connection = Connection::new(uri).await;
     let channel = connection.create_channel().await;
 
+    let retry_1_exchange_name = Self::get_retry_exchange_name(exchange_name, 1);
+    let retry_2_exchange_name = Self::get_retry_exchange_name(exchange_name, 2);
+    let wait_queue_name = Self::get_wait_queue_name(queue_name);
+
     // the main exchange that will route the messages to the queue
     Self::create_exchange(&channel, exchange_name, ExchangeKind::Direct).await;
     // the retry DLX that will accept messages when the consumer rejects a message
-    Self::create_exchange(&channel, Self::get_retry_exchange_name(exchange_name, 1), ExchangeKind::Topic).await;
+    Self::create_exchange(&channel, &retry_1_exchange_name, ExchangeKind::Topic).await;
     // the retry DLX that will accept messages that will be ttl'ed from the temporary wait queue
-    Self::create_exchange(&channel, Self::get_retry_exchange_name(exchange_name, 2), ExchangeKind::Topic).await;
+    Self::create_exchange(&channel, &retry_2_exchange_name, ExchangeKind::Topic).await;
 
     // create the given queue and set the retry 1 exchange as its DLX
     let mut args = FieldTable::default();
-    let dlx = Into::<ShortString>::into(Self::get_retry_exchange_name(exchange_name, 1));
+    let dlx = Into::<ShortString>::into(retry_1_exchange_name.clone());
+    // https://www.rabbitmq.com/dlx.html
     args.insert("x-dead-letter-exchange".into(), dlx.into());
-    let queue = Self::create_queue(&channel, queue_name, args).await;
+    let _main_queue = Self::create_queue(&channel, queue_name, args).await;
 
     // bind to the original exchange to the main queue
+    Self::queue_bind(&channel, exchange_name, queue_name, binding_key).await;
 
-    todo!()
+    // bind the retry DLX exchange to the main queue so messages that need a retry will be routed to it
+    // to be next picked up by the consumer for another processing attempt
+    Self::queue_bind(&channel, &retry_2_exchange_name, queue_name, ANY_MESSAGE).await;
+
+    // create a new temporary wait queue where messaged that are rejected will sit for some short time
+    // before they're routed via the DLX retry 2 to the main queue for a retry
+    let mut args = FieldTable::default();
+    let dlx = Into::<ShortString>::into(retry_2_exchange_name);
+    args.insert("x-dead-letter-exchange".into(), dlx.into());
+    // https://www.rabbitmq.com/ttl.html
+    args.insert("x-message-ttl".into(), ttl.into());
+    let _wait_queue = Self::create_queue(&channel, &wait_queue_name, args).await;
+
+    // finally, bind the wait queue to the retry DLX where messages that are rejected are sent back to the main queue
+    Self::queue_bind(&channel, &retry_1_exchange_name, &wait_queue_name, ANY_MESSAGE).await;
+    
+    Self {}
   }
 
   async fn create_exchange(
@@ -99,7 +122,11 @@ impl RetryConsumer {
     ).await.expect(&format!("cannot bind {} queue to exchange {}", queue_name, exchange_name));
   }
 
-  fn get_retry_exchange_name(exchange_name: &str, count: u8,) -> &str {
-    &format!("{}dlx_retry_{}", exchange_name, count)
+  fn get_retry_exchange_name(exchange_name: &str, count: u8,) -> String {
+    format!("{}dlx_retry_{}", exchange_name, count)
+  }
+
+  fn get_wait_queue_name(queue_name: &str) -> String {
+    format!("{}.wait_retry", queue_name)
   }
 }
