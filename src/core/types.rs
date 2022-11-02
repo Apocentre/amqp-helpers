e61d23c4977af5cf2e2c365b6bab486e55aa9064
ppoliani <ppoliani@gmail.com>
2022-11-02 13:30:42 +0200

    feat: add Handler to the types

--- src/core/types.rs
+++ src/core/types.rs
@@ -1,6 +1,13 @@
+use async_trait::async_trait;
+use borsh::{BorshDeserialize};
 use lapin::{
 	Result,
 	message::{Delivery},
 };
 
 pub type MessageHandler<F> = Box<dyn Fn(Result<Delivery>, i64) -> F + Send + Sync + 'static>;
+
+#[async_trait]
+pub trait Handler<M: BorshDeserialize + Send + Sync> {
+  async fn handle(&self, model: M, delivery: &Delivery) -> eyre::Result<()>;
+}
