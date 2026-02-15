0ce7cfb0afe50abc83d0baf5f7f7daa44ea86ce2
ppoliani <ppoliani@gmail.com>
2026-02-15 12:17:23 +0200

    feat: expose lapin interdace

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 pub mod core;
 pub mod consumer;
 pub mod producer;
-pub use lapin::{
-  Result as LapinResult, message::Delivery, Error as LapinError,
-  options::{BasicAckOptions, BasicNackOptions},
-};
+pub use lapin::*;
